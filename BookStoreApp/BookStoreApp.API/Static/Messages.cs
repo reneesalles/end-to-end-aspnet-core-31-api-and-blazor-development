4a6ff1c4e7da2a6ad599dd51e8e92a09c6e172e9
reneesalles <reneesalles@hotmail.com>
2022-04-19 19:04:24 -0300

    Added Static Messages (500 error message)

---
+++ BookStoreApp/BookStoreApp.API/Static/Messages.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreApp.API.Static
+{
+    public static class Messages
+    {
+        public const string Error500Message = "There was an error completing your request. Please try again later.";
+    }
+}
